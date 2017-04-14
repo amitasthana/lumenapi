9e4643eb9fca6925f86a1732727e36de7463f801
Amit Asthana <amitasthana.777@gmail.com>
2017-04-14 18:22:03 +0530

    writing test cases

--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -3,7 +3,7 @@
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class UrlTest extends TestCase
 {
     /**
      * A basic test example.
